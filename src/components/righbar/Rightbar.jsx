9e78efd5e4db576f50ceb87adfba70dee1b28bc2
Nikhil Kumar <developernik18@gmail.com>
2022-06-21 13:26:18 +0530

    create rightbar for social media app

--- src/components/righbar/Rightbar.jsx
+++ src/components/righbar/Rightbar.jsx
@@ -3,7 +3,49 @@ import './rightbar.css'
 export default function Rightbar() {
   return (
     <div className='rightbar'>
-      Rightbar
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img src="/assets/gift.png" alt="" className="birthdayImg" />
+          <span className='birthdayText'> 
+            <b>Greg wilson</b> and <b>3 other friends</b> have birthday today </span>
+        </div>
+
+        <img src="/assets/ad.jpg" alt="" className="rightbarAd" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          <div className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="/assets/person/3.jpg" alt="" className="rightbarProfileImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Selina Kyle</span>
+          </div>
+
+          <div className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="/assets/person/3.jpg" alt="" className="rightbarProfileImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Selina Kyle</span>
+          </div>
+
+          <div className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="/assets/person/3.jpg" alt="" className="rightbarProfileImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Selina Kyle</span>
+          </div>
+
+          <div className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="/assets/person/3.jpg" alt="" className="rightbarProfileImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Selina Kyle</span>
+          </div>
+        </ul>
+      </div>
     </div>
   )
 }
