f32bc8496352788b0c3021d38d2c07f42ff40373
Nikhil Kumar <developernik18@gmail.com>
2022-06-24 13:02:00 +0530

    created login page

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,28 @@
+import "./login.css"
+
+export default function Login() {
+  return (
+    <div className="login">
+      <div className="loginWrapper">
+        <div className="loginLeft">
+          <div className="loginLogo">
+            NewSocial
+          </div>
+          <span className="loginDesc">
+            Connect with friends and the world on NewSocial.
+          </span>
+        </div>
+        <div className="loginRight">
+          <div className="loginBox">
+            <input placeholder="Email"  type="Email" className="loginInput" />
+            <input placeholder="Password" type="Password" className="loginInput" />
+            <button className="loginButton">Log In</button>
+            <span className="loginForgot">Forgot Password?</span> 
+            <button className="loginRegisterButton">Create a new Account</button>           
+          </div>
+        </div>
+      </div>
+    
+    </div>
+  )
+}
