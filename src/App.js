77c3b3688a8cef2b937f9d26e2993919a9025e00
Nikhil Kumar <developernik18@gmail.com>
2022-06-25 11:27:53 +0530

    Added z-index to topbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Home from "./pages/home/Home";
 // import Profile from "./pages/profile/Profile";
-// import Register from "./pages/register/register";
+// import Register from "./pages/register/Register";
 // import Login from "./pages/login/Login";
 
 function App() {
