e509d8207748cdacf7c043a3d561e2f2377ec161
Nikhil Kumar <developernik18@gmail.com>
2022-06-29 13:31:13 +0530

    Using params to get username and update user value for feed ui

--- src/components/righbar/Rightbar.jsx
+++ src/components/righbar/Rightbar.jsx
@@ -2,7 +2,7 @@ import './rightbar.css'
 import { Users } from "../../dummyData";
 import Online from '../online/Online';
 
-export default function Rightbar({profile}) {
+export default function Rightbar({ user }) {
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
 
   const HomeRightbar = () => {
@@ -32,15 +32,19 @@ export default function Rightbar({profile}) {
         <div className="rightbarInfo">
           <div className="rightbarInfoItem">
             <span className="rightbarInfoKey">City:</span>
-            <span className="rightbarInfoValue">Newyork</span>
+            <span className="rightbarInfoValue">{user.city}</span>
           </div>
           <div className="rightbarInfoItem">
             <span className="rightbarInfoKey">From:</span>
-            <span className="rightbarInfoValue">Madrid</span>
+            <span className="rightbarInfoValue">{user.from}</span>
           </div>
           <div className="rightbarInfoItem">
             <span className="rightbarInfoKey">Relationship:</span>
-            <span className="rightbarInfoValue">Single</span>
+            <span className="rightbarInfoValue">
+              {user.relationship === 1 ? "Single" 
+                : user.relationship === 0 ? "Married" 
+                : "Complicated"}
+            </span>
           </div>
         </div>
         <div className="rightbarTitle">User Friends</div>
@@ -65,7 +69,7 @@ export default function Rightbar({profile}) {
   return (
     <div className='rightbar'>
       <div className="rightbarWrapper">
-        {profile ? <ProfileRightbar /> : <HomeRightbar />}
+        { user ? <ProfileRightbar /> : <HomeRightbar />}
       </div>
     </div>
   )
--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -3,9 +3,23 @@ import Rightbar from "../../components/righbar/Rightbar"
 import Sidebar from "../../components/sidebar/Sidebar"
 import Topbar from "../../components/topbar/Topbar"
 import "./profile.css"
+import { useState, useEffect } from "react";
+import axios from 'axios';
+import { useParams } from "react-router";
 
 export default function Profile() {
+  const [user, setUser] = useState({});
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const username = useParams().username;
+
+  useEffect (() => {
+    const fetchUser = async () => {
+      const res = await axios.get(`/users?username=${username}`);
+      setUser(res.data);
+    }
+    fetchUser();
+  }, [username]);
+
 
   return (
     <>
@@ -15,17 +29,25 @@ export default function Profile() {
         <div className="profileRight">
           <div className="profileRightTop">
             <div className="profileCover">
-              <img src={PF + "post/2.jpg"} alt="" className="profileCoverImg" />
-              <img src={PF + "person/1.jpg"} alt="" className="profileUserImg" />
+              <img 
+                src={user.coverPicture || PF + "post/2.jpg"} 
+                alt="" 
+                className="profileCoverImg"   
+              />
+              <img 
+                src={user.profilePicture || PF + "person/noAvatar.png"} 
+                alt="" 
+                className="profileUserImg" 
+              />
             </div>
             <div className="profileInfo">
-              <h4 className="profileInfoName">Sam William</h4>
-              <span className="profileInfoDesc">Hello to all my friends</span>
+              <h4 className="profileInfoName">{user.username}</h4>
+              <span className="profileInfoDesc"> {user.desc} </span>
             </div>
           </div>
           <div className="profileRightBottom">
-            <Feed username="person2"/>
-            <Rightbar profile/>
+            <Feed username={username}/>
+            <Rightbar user={user}/>
           </div>
         </div>
       </div>
