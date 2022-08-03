4c95d7a8d4e3fd804db4565682ba72fc37e45d97
Nikhil Kumar <developernik18@gmail.com>
2022-08-03 17:09:35 +0530

    Redirect on login and show login if user not present

--- src/App.js
+++ src/App.js
@@ -6,15 +6,20 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
+  Navigate
 } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
+  const {user} = useContext(AuthContext);
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/" element={user ? <Home /> : <Login />} />
+        <Route path="/login" element={user? <Navigate to="/" /> : <Login />} />
+        <Route path="/register" element={user? <Navigate to="/" /> : <Register />} />
         <Route path="/profile/:username" element={<Profile />} />
       </Routes>
     </Router>
