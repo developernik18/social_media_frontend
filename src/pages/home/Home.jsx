9b47e34e9617b23fc4ef0667c55f639ad82793a3
Nikhil Kumar <developernik18@gmail.com>
2022-08-06 16:40:24 +0530

    using http-proxy-middleware plugin to setup proxy

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import Feed from "../../components/feed/Feed";
 import Rightbar from "../../components/righbar/Rightbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Topbar from "../../components/topbar/Topbar";
+import React from 'react';
 
 import "./home.css";
 
