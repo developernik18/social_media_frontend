3cf345f302b47b831b2b78fe20ea35f7bddfb753
Nikhil Kumar <developernik18@gmail.com>
2022-08-10 11:31:12 +0530

    Added like functionality

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,23 +1,25 @@
 import Post from "../post/Post";
 import Share from "../share/Share";
 import "./feed.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import axios from 'axios';
+import { AuthContext } from "../../context/AuthContext";
 
 export default function Feed({username}) {
   const [posts, setPosts] = useState([]);
+  const {user} = useContext(AuthContext);
 
   useEffect (() => {
     const fetchPosts = async () => {
       const response = username 
         ? await axios.get("/posts/profile/" + username) 
-        : await axios.get("/posts/timeline/62ad69b81d7eda68323a4965");
+        : await axios.get("/posts/timeline/" + user._id);
       setPosts(response.data);
     }
 
     fetchPosts();
 
-  }, [username]);
+  }, [username, user._id]);
 
   return (
     <div className="feed">
--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -5,12 +5,19 @@ import axios from 'axios';
 // import { format } from 'timeago.js'
 import TimeAgo from 'timeago-react';
 import { Link } from 'react-router-dom';
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
 
 export default function Post({post}) {
   const [like, setLike] = useState(post.likes.length);
   const [isLiked, setIsLiked] = useState(false);
   const [user, setUser] = useState({});
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const {user: currentUser} = useContext(AuthContext);
+
+  useEffect(() => {
+    setIsLiked(post.likes.includes(currentUser._id));
+  }, [currentUser._id, post.likes]);
 
   useEffect (() => {
     const fetchUser = async () => {
@@ -23,6 +30,11 @@ export default function Post({post}) {
   }, [post.userId]);
 
   const handleLike = () => {
+    try {
+      axios.put("/posts/"+post._id+"/like", {userId: currentUser._id});
+    } catch (error) {
+      
+    }
     setLike(isLiked ? like - 1 : like + 1);
     setIsLiked(!isLiked);
   }
@@ -33,7 +45,7 @@ export default function Post({post}) {
         <div className="postTop">
           <div className="postTopLeft">
             <Link to={`/profile/${user.username}`}>
-              <img src={user.profilePicture || PF + "person/noAvatar.png"} 
+              <img src={user.profilePicture ? PF + user.profilePicture : PF + "person/noAvatar.png"} 
                 alt="" className="postProfileImg"/>
             </Link>
             <Link to={`/profile/${user.username}`} style={{textDecoration: 'none', color:'#555'}}>
