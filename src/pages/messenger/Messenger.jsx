724a1196fec2cc4da70a5de6b5b459b30972759a
Nikhil Kumar <developernik18@gmail.com>
2022-09-11 14:28:40 +0530

    implementation of socket to send and receive messages

--- src/pages/messenger/Messenger.jsx
+++ src/pages/messenger/Messenger.jsx
@@ -13,13 +13,40 @@ export default function Messenger() {
   const [currentChat, setCurrentChat] = useState(null);
   const [messages, setMessages] = useState(null);
   const [newMessage, setNewMessage] = useState("");
-  const [socket, setSocket] = useState(null);
+  const [arrivalMessage, setArrivalMessage] = useState(null);
+  const socket = useRef();
   const { user } = useContext(AuthContext);
   const scrollRef = useRef();
 
   useEffect(() => {
-    setSocket(io("ws://localhost:8900"))
-  }, []);
+    console.log('check');
+    socket.current = io("ws://localhost:8900");
+    socket.current.on('getMessage', data => {
+      setArrivalMessage({
+        sender: data.senderId,
+        text:data.text,
+        createdAt: Date.now()
+      })
+    })
+  },[]);
+
+  useEffect(() => {
+    arrivalMessage && currentChat?.members.includes(arrivalMessage.sender) &&
+      setMessages(prev => [...prev, arrivalMessage]);
+  }, [arrivalMessage, currentChat]);
+
+  useEffect(() => {
+    socket.current.emit('addUser', user._id);
+    socket.current.on("getUsers", users => {
+      console.log(users);
+    })
+  },[user])
+
+  // useEffect(() => {
+  //   socket.current?.on("welcome", message => {
+  //     console.log(message);
+  //   });
+  // }, [socket]);
 
   useEffect(() => {
     const getConversations = async () => {
@@ -51,6 +78,8 @@ export default function Messenger() {
     scrollRef.current?.scrollIntoView({behavior: "smooth"});
   },[messages])
 
+
+
   const handleSubmit =async (e) => {
     e.preventDefault();
     const message = {
@@ -58,6 +87,16 @@ export default function Messenger() {
       text: newMessage,
       conversationId: currentChat._id
     }
+
+    const receiverId = currentChat.members.find(
+      member => member !== user._id
+    );
+
+    socket.current.emit("sendMessage", {
+      senderId: user._id,
+      receiverId,
+      text: newMessage
+    })
 
     try {
       const res = await axios.post('messages', message);
